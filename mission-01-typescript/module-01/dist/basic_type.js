8d87e1e4896daa0902896a70bcd65ebedd07c62a
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2023-11-03 14:47:58 +0600

    basic types

---
+++ mission-01-typescript/module-01/dist/basic_type.js
@@ -0,0 +1,13 @@
+"use strict";
+let person = "Sabbir Ahmmed";
+console.log(person);
+// array of number
+const numArr = [1, 2, 3];
+numArr.push(10);
+// array of string
+const stringArr = ['sabbir', 'ahmmed'];
+stringArr.push('md: ');
+// array of fixed number and string combination
+const combinationArr = ['sabbir', 185];
+combinationArr[0] = 'Sabbir Ahmmed';
+combinationArr[1] = 29;
---
+++ mission-01-typescript/module-01/src/basic_type.ts
@@ -0,0 +1,15 @@
+let person: string = "Sabbir Ahmmed"
+console.log(person);
+
+// array of number
+const numArr: number[] = [1,2,3];
+numArr.push(10)
+
+// array of string
+const stringArr: string[] = ['sabbir', 'ahmmed'];
+stringArr.push('md: ')
+
+// array of fixed number and string combination
+const combinationArr: [string, number] = ['sabbir', 185];
+combinationArr[0] = 'Sabbir Ahmmed';
+combinationArr[1] = 29;
