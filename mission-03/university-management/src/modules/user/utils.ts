a64194a0359c891382ca370284c59acfe4348b5f
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2023-12-08 13:12:01 +0600

    id generate logic updated

--- mission-03/university-management/src/modules/user/utils.ts
+++ mission-03/university-management/src/modules/user/utils.ts
@@ -12,7 +12,32 @@ export const findLastStudentId = async () => {
     return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
 }
 
+// each semester id will be regenerated from 0001
+// check by semester code and year from the previous student
 export const generateStudentId = async (payload: TAcademicSemester | any) => {
+    let currentId = (0).toString();
+    const lastStudentId = await findLastStudentId(); // 2023 01 0001
+
+    const lastStudentSemesterCode = lastStudentId?.substring(4, 6); // 01
+    const lastStudentYear = lastStudentId?.substring(0, 4); // 2023
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    if(
+        lastStudentId &&
+        lastStudentSemesterCode === currentSemesterCode &&
+        lastStudentYear === currentYear
+    ) {
+        currentId = lastStudentId.substring(6) // 0001
+    }
+
+    let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
+    incrementId = `${payload.year}${payload.code}${incrementId}`;
+    return incrementId;
+}
+
+// always increment the student number
+export const simplePatternGenerateStudentId = async (payload: TAcademicSemester | any) => {
     const currentId = (await findLastStudentId()) || (0).toString();
     let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
     incrementId = `${payload.year}${payload.code}${incrementId}`;
