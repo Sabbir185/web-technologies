6e1e1708bd775819d789c4fafe1faf97db7f0065
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2024-01-21 23:51:52 +0600

    code refactory for routes and sidebar

--- full-stack/05-phum-full-stack/src/components/layout/MainLayout.tsx
+++ full-stack/05-phum-full-stack/src/components/layout/MainLayout.tsx
@@ -1,39 +1,14 @@
-import { Layout, Menu } from 'antd';
+import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
-import { adminSidebarItems } from '../../routes/admin.routes';
-
-const { Header, Content, Footer, Sider } = Layout;
-
+import Sidebar from './Sidebar';
+const { Header, Content } = Layout;
 
 const MainLayout = () => {
     return (
-        <Layout style={{height: '100vh'}}>
-            <Sider
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                    console.log(collapsed, type);
-                }}
-            >
-                <div
-                    style={{
-                        color: 'white',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        height: '3.6rem'
-                    }} 
-                >
-                    <h1>PHU</h1>
-                </div>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={adminSidebarItems} />
-            </Sider>
+        <Layout style={{ height: '100vh' }}>
+            <Sidebar />
             <Layout>
                 <Header style={{ padding: 0 }} />
-
                 <Content style={{ margin: '24px 16px 0' }}>
                     <div
                         style={{
@@ -44,10 +19,6 @@ const MainLayout = () => {
                         <Outlet />
                     </div>
                 </Content>
-
-                <Footer style={{ textAlign: 'center' }}>
-                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
-                </Footer>
             </Layout>
         </Layout>
     );
--- full-stack/05-phum-full-stack/src/routes/admin.routes.tsx
+++ full-stack/05-phum-full-stack/src/routes/admin.routes.tsx
@@ -1,22 +1,10 @@
-import { NavLink } from "react-router-dom";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateAdmin from "../pages/admin/CreateAdmin";
 import CreateFaculty from "../pages/admin/CreateFaculty";
 import CreateStudent from "../pages/admin/CreateStudent";
-import { ReactNode } from "react";
 
-// types
-type TSidebarItem = {
-    key: string;
-    label: ReactNode;
-    children?: TSidebarItem[];
-};
-type TRoute = {
-    path: string;
-    element: ReactNode;
-};
 
-const adminPaths = [
+export const adminPaths = [
     {
         name: 'Dashboard',
         path: 'dashboard',
@@ -46,45 +34,45 @@ const adminPaths = [
 
 
 //* admin sidebar items
-export const adminSidebarItems = adminPaths?.reduce((acc: TSidebarItem[], item) => {
-    if (item.path && item.name) {
-        acc.push({
-            key: item.name,
-            label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
-        })
-    }
-    if (item.children) {
-        acc.push({
-            key: item.name,
-            label: item.name,
-            children: item.children.map((child) => ({
-                key: child.name,
-                label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
-            }))
-        })
-    }
-    return acc;
-}, []);
+// export const adminSidebarItems = adminPaths?.reduce((acc: TSidebarItem[], item) => {
+//     if (item.path && item.name) {
+//         acc.push({
+//             key: item.name,
+//             label: <NavLink to={`/admin/${item.path}`}>{item.name}</NavLink>
+//         })
+//     }
+//     if (item.children) {
+//         acc.push({
+//             key: item.name,
+//             label: item.name,
+//             children: item.children.map((child) => ({
+//                 key: child.name,
+//                 label: <NavLink to={`/admin/${child.path}`}>{child.name}</NavLink>
+//             }))
+//         })
+//     }
+//     return acc;
+// }, []);
 
 
 //* admin routes
-export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
-    if(item.path && item.element) {
-        acc.push({
-            path: item.path,
-            element: item.element,
-        })
-    }
-    if(item.children) {
-        item.children.forEach((child) => {
-            acc.push({
-                path: child.path,
-                element: child.element
-            })
-        })
-    }
-    return acc;
-}, [])
+// export const adminRoutes = adminPaths.reduce((acc: TRoute[], item) => {
+//     if(item.path && item.element) {
+//         acc.push({
+//             path: item.path,
+//             element: item.element,
+//         })
+//     }
+//     if(item.children) {
+//         item.children.forEach((child) => {
+//             acc.push({
+//                 path: child.path,
+//                 element: child.element
+//             })
+//         })
+//     }
+//     return acc;
+// }, [])
 
 //! Hard coded way
 // export const adminPaths = [
